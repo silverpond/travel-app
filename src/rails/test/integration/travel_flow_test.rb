40663c75544cf5238e7d35a30a646d1b71a38267
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-23 16:53:12 +1100

    Added test for initilal search and some basic data in fixtures

--- src/rails/test/integration/travel_flow_test.rb
+++ src/rails/test/integration/travel_flow_test.rb
@@ -4,14 +4,18 @@ require 'integration_test_helper'
 class TravelFlowTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-  
   test 'getting to homepage' do
     visit '/'
     assert page.has_content? 'Travel All'
   end
-  
+
+  test 'submit from and to airports and see next page with more info' do
+    visit '/'
+    fill_in 'From', :with => 'MEL'
+    fill_in 'To', :with => 'KIX'
+    click_button_or_line 'Fly'
+    assert page.has_content? 'Kansai International Airport'
+    assert page.has_content? 'Tullamarine International Airport'
+  end
+
 end
