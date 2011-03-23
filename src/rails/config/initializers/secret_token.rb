2ba2112761eb94e10957ab3837e9a6bb024ffa2d
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-23 15:18:20 +1100

    Added Rails

---
+++ src/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelApp::Application.config.secret_token = '282e390506cce94deec31ff22f3543c6fefd270c1a78e6d4f71497eacdb10728e876a3592ee16014f682ca3d77aa124f4ef5fb458739c8d8f217d381f564bcb3'
