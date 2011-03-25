aca5cdccf43e7bbc2653214f2a8b618a6ed0ff6a
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-25 14:41:47 +1100

    Very fancy Search for Airport

--- src/rails/app/models/airport.rb
+++ src/rails/app/models/airport.rb
@@ -1,2 +1,21 @@
 class Airport < ActiveRecord::Base
+  def self.search q
+    conditions = {}
+    for term in q.split ' '
+      if conditions == {}
+        conditions = { :name.matches => "%#{term}%" } |
+                      { :code.matches => "%#{term}%" } |
+                      { :city.matches => "%#{term}%" } |
+                      { :country.matches => "%#{term}%" }
+        else
+        conditions = conditions | { :name.matches => "%#{term}%" }
+        conditions = conditions | { :code.matches => "%#{term}%" }
+        conditions = conditions | { :city.matches => "%#{term}%" }
+        conditions = conditions | { :country.matches => "%#{term}%" }
+      end
+
+
+    end
+    where(conditions)
+  end
 end
