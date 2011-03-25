95a2adb30f35ee7f82e9a0cd0b5be075284d58e7
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-25 14:47:18 +1100

    Changed Lable for Autocomplete

--- src/rails/app/controllers/airports_controller.rb
+++ src/rails/app/controllers/airports_controller.rb
@@ -2,7 +2,7 @@ class AirportsController < ApplicationController
 
   def autocomplete_by_name
     @airports = Airport.search(params[:term]).limit(10)
-    render :json => @airports.map{|x| { :label => x.name, :value => x.code } }
+    render :json => @airports.map{|x| { :label => "#{x.name}, #{x.country}(#{x.code})", :value => x.code } }
   end
 
 end
