206e3f59dc7cdfeec16aa5c13cc315ea4f846965
Jono Chang <jonathan.chang@silverpond.com.au>
2011-03-27 12:58:26 +1100

    added open flights airports database and using this as the default airports data

--- src/rails/db/import.rb
+++ src/rails/db/import.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require_relative '../config/environment'
 
+# imports data downloaded from http://www.partow.net/miscellaneous/airportdatabase/
 def import_global_airports_database
   airports_text = IO.read('db/imports/GlobalAirportDatabase.txt')
 
@@ -16,6 +17,7 @@ def import_global_airports_database
     airport.city = city
     airport.country = country
 
+    #convert from radians to degrees
     airport.latitude = lat_d.to_f  + ((lat_m.to_f + (lat_s.to_f / 60.0)) / 60.0)
     airport.longitude = long_d.to_f + ((long_m.to_f + (long_s.to_f / 60.0)) / 60.0)
 
@@ -27,13 +29,32 @@ def import_global_airports_database
     airport.save!
 
   end
-
 end
 
+# imports data downloaded from http://www.openflights.org/data.html
+def import_open_flights_airports
+  CSV.foreach('db/imports/open_flights_airports_database.csv') do |row|
+
+    id, name, city, country, code3, code4, lat, lng, alt, timezone, dst = row
+
+    airport = Airport.new(
+      :code => code3.upcase,
+      :name => name.upcase,
+      :city => city.upcase,
+      :country => country.upcase,
+      :latitude => lat,
+      :longitude => lng,
+      :altitude => alt,
+      :timezone => timezone,
+      :daylight_savings_zone => dst
+    )
+
+    puts airport.inspect
+    airport.save!
+  end
+end
 
 def import_currencies
-  airports_text = IO.read('db/imports/dl_iso_table_a1.csv')
-
   CSV.foreach 'db/imports/dl_iso_table_a1.csv' do |row|
     country,currency_name,currency_code,numeric_code,minor_unit = row
 
@@ -48,20 +69,53 @@ def import_currencies
   end
 end
 
-
+def update_currency( set_country, where_country )
+  sql = ActiveRecord::Base.connection
+  sql.execute "UPDATE currencies 
+               SET country = '#{set_country}' 
+               WHERE country = '#{where_country}'"
+end
 
 Airport.transaction do
   puts "importing airports..."
-  import_global_airports_database
+  import_open_flights_airports
 
-  #cleanup data to be the same as currency
-  sql = ActiveRecord::Base.connection
-  sql.execute "UPDATE airports 
-               SET country = 'USA' 
-               WHERE country = 'UNITED STATES'"
 end
 
 Currency.transaction do
   puts "importing currencies..."
   import_currencies
+  #cleanup data to be the same as airports
+  
+  puts "updating currency countries"
+  update_currency('BOLIVIA', 'BOLIVIA, PLURINATIONAL STATE OF')
+  update_currency('BRUNEI', 'BRUNEI DARUSSALAM')
+  update_currency('BOSNIA AND HERZEGOVINA', 'BOSNIA & HERZEGOVINA')
+  update_currency('CONGO (BRAZZAVILLE)', 'CONGO')
+  update_currency('CONGO (KINSHASA)', 'CONGO, THE DEMOCRATIC REPUBLIC OF')
+  update_currency('EAST TIMOR', 'TIMOR-LESTE')
+  update_currency('FALKLAND ISLANDS', 'FALKLAND ISLANDS (MALVINAS)')
+  update_currency('IRAN', 'IRAN, ISLAMIC REPUBLIC OF')
+  update_currency('NORTH KOREA', 'KOREA, DEMOCRATIC PEOPLE\\\'S REPUBLIC OF')
+  update_currency('SOUTH KOREA', 'KOREA, REPUBLIC OF')
+  update_currency('LAOS', 'LAO PEOPLE\\\'S DEMOCRATIC REPUBLIC')
+  update_currency('LIBYA', 'LIBYAN ARAB JAMAHIRIYA')
+  update_currency('MACAU', 'MACAO')
+  update_currency('MACEDONIA', 'MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF')
+  update_currency('MICRONESIA', 'MICRONESIA, FEDERATED STATES OF')
+  update_currency('MOLDOVA', 'MOLDOVA, REPUBLIC OF')
+  update_currency('PALESTINE', 'PALESTINIAN TERRITORY, OCCUPIED')
+  update_currency('RUSSIA', 'RUSSIAN FEDERATION')
+  update_currency('SAINT HELENA', 'SAINT HELENA, ASCENSION AND TRISTAN DA CUNHA')
+  update_currency('SOUTH GEORGIA AND THE ISLANDS', 'SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS')
+  update_currency('SVALBARD', 'SVALBARD AND JAN MAYEN')
+  update_currency('SYRIA', 'SYRIAN ARAB REPUBLIC')
+  update_currency('TAIWAN', 'TAIWAN, PROVINCE OF CHINA')
+  update_currency('TANZANIA', 'TANZANIA, UNITED REPUBLIC OF')
+  update_currency('VENEZUELA', 'VENEZUELA, BOLIVARIAN REPUBLIC OF')
+  update_currency('VIETNAM', 'VIET NAM')
+
+  update_currency('BRITISH VIRGIN ISLANDS', 'VIRGIN ISLANDS (BRITISH)')
+  update_currency('VIRGIN ISLANDS', 'VIRGIN ISLANDS (US)')
+
 end
