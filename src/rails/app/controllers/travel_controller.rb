0487f307ffaad65cd3c2c54b1d0a3eebfecc52cb
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-29 09:29:36 +1100

    error validations on travel page

--- src/rails/app/controllers/travel_controller.rb
+++ src/rails/app/controllers/travel_controller.rb
@@ -7,6 +7,13 @@ class TravelController < ApplicationController
     @from_airport = Airport.where(:code => params[:travel][:from]).first
     @to_airport = Airport.where(:code => params[:travel][:to]).first
 
+    #consider refactoring to Travel class
+    flash[:error] = []
+    flash[:error] << 'Could not find departure airport' if !@from_airport
+    flash[:error] << 'Could not find arrival airport' if !@to_airport
+    render 'index' and return if flash[:error] != []
+
+
     weather_service = Service::WorldWeatherOnline.new @to_airport.latitude,@to_airport.longitude, 5
     @weather_days = weather_service.weather_days
 
