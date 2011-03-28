18ba563b51c83ff7bfdc517de8680122ccc68914
Jono Chang <jonathan.chang@silverpond.com.au>
2011-03-28 20:22:33 +1100

    added airports

--- src/rails/test/blueprints.rb
+++ src/rails/test/blueprints.rb
@@ -7,3 +7,23 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+
+Airport.blueprint(:MEL) do
+  code { "MEL" }
+  name { "MELBOURNE INTL" }
+  city { "MELBOURNE" }
+  country { "AUSTRALIA" }
+  latitude { "-37.673333" }
+  longitude { "144.843333" }
+end
+
+Airport.blueprint(:LAX) do
+  code { "LAX" }
+  name { "LOS ANGELES INTL" }
+  city { "LOS ANGELES" }
+  country { "UNITED STATES" }
+  latitude { "33.942536" }
+  longitude { "-118.408075" }
+end
+
