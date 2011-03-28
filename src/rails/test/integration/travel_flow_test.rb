f3d577527c34b79908d57aa2346dfa0825741e4d
Kirill Radzikhovskyy <kirillrdy@silverpond.com.au>
2011-03-29 08:58:36 +1100

    Updated Integration tests to work with recent changes

--- src/rails/test/integration/travel_flow_test.rb
+++ src/rails/test/integration/travel_flow_test.rb
@@ -11,11 +11,11 @@ class TravelFlowTest < ActionDispatch::IntegrationTest
 
   test 'submit from and to airports and see next page with more info' do
     visit '/'
-    fill_in 'From', :with => 'MEL'
-    fill_in 'To', :with => 'KIX'
+    fill_in 'travel_from', :with => 'MEL'
+    fill_in 'travel_to', :with => 'KIX'
     click_button 'Fly'
     assert page.has_content? 'Kansai International Airport'
-    assert page.has_content? 'Tullamarine International Airport'
+    assert page.has_content? 'MELBOURNE International Airport'
   end
 
 end
